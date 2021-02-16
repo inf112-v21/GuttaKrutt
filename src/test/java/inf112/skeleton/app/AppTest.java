ea03bf25739b6ea7d832c185b397fe5b96f5969c
Asle Apeland <asle.apeland@student.uib.no>
2021-02-16 13:50:07 +0100

    Første draft til unit-testing av spillet

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -1,17 +1,39 @@
 package inf112.skeleton.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
 import org.junit.Test;
+
+import java.awt.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
+    public Game setup () {
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+        cfg.setTitle("RoboRally");
+        cfg.setWindowedMode(500, 500);
+
+        Game testGame = new Game();
+        Lwjgl3Application app = new Lwjgl3Application(testGame, cfg);
+        app.exit();
+        return testGame;
+    }
+
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+
+        Game a = setup();
+        Vector2 truePos = new Vector2(0,0);
+
+        assertEquals(a.playerPos,truePos);
+        //assertEquals(0,1);
     }
 }
