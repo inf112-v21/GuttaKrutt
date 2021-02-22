935aa54caae98fbf412504dcdcaf69bd6c4e5712
Simon-Berg <simonkb98@hotmail.com>
2021-02-22 16:50:52 +0100

    Started working on GameServer and GameClient

--- src/main/java/inf112/skeleton/app/GameClient.java
+++ src/main/java/inf112/skeleton/app/GameClient.java
@@ -1,2 +1,15 @@
-package inf112.skeleton.app;public class GameClient {
+package inf112.skeleton.app;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.minlog.Log;
+
+public class GameClient {
+    Client client;
+    String name;
+
+    public GameClient () {
+        client = new Client();
+        client.start();
+    }
 }
--- src/main/java/inf112/skeleton/app/GameServer.java
+++ src/main/java/inf112/skeleton/app/GameServer.java
@@ -1,2 +1,26 @@
-package inf112.skeleton.app;public class GameServer {
+package inf112.skeleton.app;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.kryo.Kryo;
+
+import java.io.IOException;
+
+public class GameServer {
+    Server server;
+
+    public GameServer () throws IOException {
+        server = new Server() {
+            protected Connection newConnection () {
+                // By providing our own connection implementation, we can store per
+                // connection state without a connection ID to state look up.
+                return new GameConnection();
+            }
+        };
+    }
+
+    static class GameConnection extends Connection {
+        public String name;
+    }
+
 }
