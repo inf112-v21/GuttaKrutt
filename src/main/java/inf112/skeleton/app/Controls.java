8cbe724e5a6b868a38bc24b585ac279faeff9d58
Asle Apeland <asle.apeland@student.uib.no>
2021-03-02 16:28:20 +0100

    Revert "Fjernet muligheten for robot å plassere laser"
    This reverts commit 68b220a9

--- src/main/java/inf112/skeleton/app/Controls.java
+++ src/main/java/inf112/skeleton/app/Controls.java
@@ -57,6 +57,10 @@ public class Controls extends InputAdapter {
             robots[0].fireLaser = 20;
             return true;
         }
+        if (keyCode == Input.Keys.I) {
+            map[4][robots[0].getX()][robots[0].getY()] = 1;
+            return true;
+        }
 
         return false;
     }
