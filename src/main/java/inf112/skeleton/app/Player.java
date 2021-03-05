2f0f787ba5374de8eb842545f706fc5259631cd7
SassanZmd <60072564+SassanZmd@users.noreply.github.com>
2021-03-05 14:17:40 +0100

    programRegisters() in GameLogic and its respective test class in GameLogicTest + comments added to the following classes: GameLogic, Card, Player & Deck

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -3,11 +3,15 @@ package inf112.skeleton.app;
 import java.util.ArrayList;
 import java.util.List;
 
+/* contains all information about a player */
 public class Player {
 
     Robot robot;
-    List<Card> cardList = new ArrayList<Card>();
+    List<Card> cardList = new ArrayList<>();
+    List<Card> registers = new ArrayList<>();
 
+    /* constructor class which initiates a new robot
+    * alongside the player */
     public Player() {
         robot = new Robot();
     }
@@ -17,4 +21,9 @@ public class Player {
     }
 
     public List<Card> getCards() { return cardList; }
+
+    public List<Card> getRegisters() { return registers; }
+
+    /* programs one register at a time */
+    public void addRegister(Card c) { registers.add(c); }
 }
